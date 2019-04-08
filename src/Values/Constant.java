ac7b6bee0a7115f0dc88197b628502b1be67603e
paoloandreiseril <paolo_andrei02@yahoo.com>
2019-04-08 16:40:54 +0800

    Added code for attr. 'event' in logs

--- src/Values/Constant.java
+++ src/Values/Constant.java
@@ -26,5 +26,36 @@ public class Constant {
 
     public static Map<String, Integer> attemptCounts = new HashMap<>();
 
+    // EVENT CODES
+    public static String USER_LOCKED = "UL";
+    public static String USER_UNLOCKED = "UUL";
+
+    public static String EDIT_ROLE_SUCCESSFUL = "ER-S";
+
+    public static String CHANGED_PASSWORD_SUCCESSFUL = "CP-S";
+    public static String CHANGED_PASSWORD_FAILURE = "CP-F";
+
+    public static String DELETE_USER_SUCCESSFUL = "DU-S";
+
+    public static String REGISTER_SUCCESSFUL = "RE-S";
+    public static String REGISTER_FAILED = "RE-S";
+
+    public static String LOGIN_SUCCESSFUL = "LO-S";
+    public static String LOGIN_FAILED = "LO-F";
+
+    public static String PURCHASE_SUCCESSFUL = "PP-S";
+    public static String PURCHASE_FAILED = "PP-F";
+
+    public static String ADD_PRODUCT_SUCCESSFUL = "AP-S";
+    public static String ADD_PRODUCT_FAILED = "AP-F";
+
+    public static String REMOVE_PRODUCT_SUCCESSFUL = "RP-S";
+    public static String REMOVE_PRODUCT_FAILED = "RP-F";
+
+    public static String EDIT_PRODUCT_SUCCESSFUL = "EP-S";
+    public static String EDIT_PRODUCT_FAILED = "EP-F";
+
+    public static String DEBUG_MODE_ON = "DEBUG-M1";
+    public static String DEBUG_MODE_OFF = "DEBUG-M0";
 
 }
